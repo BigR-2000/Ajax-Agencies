277d980538174dbdef53071583c15707be3d8488
Remi De Mesel <137087632+BigR-2000@users.noreply.github.com>
2024-09-04 15:18:20 +0200

    Add files via upload

--- zaakwaarnemers_per_land.py
+++ zaakwaarnemers_per_land.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
+##Layout 
 st.set_page_config(page_title = 'Lijst zaakwaarnemers',
                    page_icon = ':briefcase:',
                    layout="wide")
@@ -18,27 +19,31 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+##titel en tekst
 def title_with_icon(icon, title):
     st.markdown(f"<div class='title-wrapper'><div class='icon'>{icon}</div><h4>{title}</h4></div>", unsafe_allow_html=True)
 
-title_with_icon('📋', "Lijst zaakwaarnemers")
-st.markdown("- Filter zaakwaarnemers op land")
+title_with_icon('', "Lijst zaakwaarnemers en agentschappen")
+st.divider()
+st.markdown("- Vind zaakwaarnemers van bepaalde landen")
 st.markdown("- Vind zaakwaarnemers van specifieke spelers")
 st.markdown("- Check of er contactgegevens beschikbaar zijn van de zaakwaarnemers")
 st.markdown("")
 
 
-
+##Inlezen data spelers en zaakwaarnemers
 try:
     lijst = pd.read_csv(f"Lijst_ZW.csv", encoding='latin1', sep=';')
     lijst_speler = pd.read_csv(f"Lijst_SP.csv", encoding='latin1', sep=';')
 except:
     print('fout bij inlading')
 
+##subset met enkele nodige data spelers
 lijst_speler = lijst_speler.rename(columns={'ï»¿Player': 'Player'})
 lijst_speler = lijst_speler[['Player', 'Club', 'Agency']]
-
-lijst2 = lijst
+lijst_speler.drop_duplicates(subset=['Player','Club'], inplace=True)
+##Goedzetten data van de zaakwaarnemers
+lijst2 = lijst 
 lijst = lijst.drop(columns = ['Fax', 'Link'])
 lijst = lijst.rename(columns={'Ã¸-Market value': 'Market value'})
 lijst = lijst.rename(columns={'ï»¿Agency name': 'Agency'})
@@ -50,49 +55,10 @@ lijst['Market value'] = lijst['Market value'].map(lambda x:x.lstrip('â¬'))
 lijst.sort_values(by=['Agency', 'Players 1st tier', 'Market value'], inplace = True)
 lijst.drop_duplicates(subset=['Agency'], keep = 'last', inplace = True)
 
+##samenbrengen data zaakwaarnemers en spelers
 lijst_spzw = pd.merge(lijst_speler, lijst, on= 'Agency', how='left')
-#st.dataframe(lijst_spzw)
 
-
-col1, col2, col3 = st.columns([3, 0.75, 3])
-with col1:
-    land = st.multiselect('Agentschap land', lijst_spzw['Country'].unique())
-    if land:
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Country'].isin(land)]
-
-with col3:        
-    telefoon = st.selectbox('Telefoonnr.', ['', 'Bekend', 'Onbekend'])
-    if telefoon == 'Bekend':
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Phone'] != '-'] 
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Phone'].notna()]
-    elif telefoon == 'Onbekend':
-        lijst_spzw = lijst_spzw[(lijst_spzw['Phone'] == '-') | (lijst_spzw['Phone'].isna())]
-    else:
-        pass
-
-    mail = st.selectbox('Email', ['', 'Bekend', 'Onbekend'])
-    if mail == 'Bekend':
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Email'] != '-'] 
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Email'].notna()]
-    elif mail == 'Onbekend':
-        lijst_spzw = lijst_spzw[(lijst_spzw['Email'] == '-') | (lijst_spzw['Email'].isna())]
-    else:
-        pass
-
-    website = st.selectbox('Website', ['', 'Bekend', 'Onbekend'])
-    if website == 'Bekend':
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Website'] != '-'] 
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Website'].notna()]
-    elif website == 'Onbekend':
-        lijst_spzw = lijst_spzw[(lijst_spzw['Website'] == '-') | (lijst_spzw['Website'].isna())]
-    else:
-        pass
-
-with col1:
-    speler = st.multiselect('Speler(s)', lijst_spzw['Player'].unique())
-    if speler:
-        lijst_spzw = lijst_spzw.loc[lijst_spzw['Player'].isin(speler)]
-
+##Rare letters veranderen naar de juiste letters
 lijst_spzw = lijst_spzw.replace('Ã©', 'é', regex=True)
 lijst_spzw = lijst_spzw.replace('Ã³', 'ó', regex=True)
 lijst_spzw = lijst_spzw.replace('Ã', 'Á', regex=True)
@@ -113,9 +79,77 @@ lijst_spzw = lijst_spzw.replace('í«', 'ë', regex=True)
 lijst_spzw = lijst_spzw.replace('í', 'Ó', regex=True)
 lijst_spzw = lijst_spzw.replace('í§', 'ç', regex=True)
 lijst_spzw = lijst_spzw.replace('í¨', 'è', regex=True)
+lijst_spzw = lijst_spzw.replace('Ä°', 'İ', regex=True)
+lijst_spzw = lijst_spzw.replace('í', 'Ü', regex=True)
+lijst_spzw = lijst_spzw.replace('Å', 'ş', regex=True)
+lijst_spzw = lijst_spzw.replace('Ä', 'ğ', regex=True)
+lijst_spzw = lijst_spzw.replace('í', 'Ç', regex=True)
 
 
+
+
+
+
+
+
+## Alle selectie boxen introduceren voor het filteren van de dataset
+col1, col2, col3 = st.columns([3, 0.75, 3])
+with col1:
+    agentschap = st.multiselect('💼 Zaakwaarnemer(s)', lijst_spzw['Agency'].unique())
+    if agentschap:
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Agency'].isin(agentschap)]
+            
+    land = st.multiselect('🌍 Land zaakwaarnemer(s)', lijst_spzw['Country'].unique())
+    if land:
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Country'].isin(land)]
+
+with col3:        
+    telefoon = st.selectbox('📞 Telefoonnr.', ['', 'Bekend', 'Onbekend'])
+    if telefoon == 'Bekend':
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Phone'] != '-'] 
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Phone'].notna()]
+    elif telefoon == 'Onbekend':
+        lijst_spzw = lijst_spzw[(lijst_spzw['Phone'] == '-') | (lijst_spzw['Phone'].isna())]
+    else:
+        pass
+
+    mail = st.selectbox('✉️ Email', ['', 'Bekend', 'Onbekend'])
+    if mail == 'Bekend':
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Email'] != '-'] 
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Email'].notna()]
+    elif mail == 'Onbekend':
+        lijst_spzw = lijst_spzw[(lijst_spzw['Email'] == '-') | (lijst_spzw['Email'].isna())]
+    else:
+        pass
+
+    website = st.selectbox('🌐 Website', ['', 'Bekend', 'Onbekend'])
+    if website == 'Bekend':
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Website'] != '-'] 
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Website'].notna()]
+    elif website == 'Onbekend':
+        lijst_spzw = lijst_spzw[(lijst_spzw['Website'] == '-') | (lijst_spzw['Website'].isna())]
+    else:
+        pass
+
+with col1:
+    speler = st.multiselect('⚽ Speler(s)', lijst_spzw['Player'].unique())
+    if speler:
+        lijst_spzw = lijst_spzw.loc[lijst_spzw['Player'].isin(speler)]
+
+
+
+##Weergave van de gefilterde dataset
 lijst_spzw = lijst_spzw.reset_index(drop=True)
 st.dataframe(lijst_spzw, height = 600)
+
+
+
+
+
+
+
+
+
+
 
 
